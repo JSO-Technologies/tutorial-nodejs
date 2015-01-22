36d28fcb07aa5c5580ee2ea82f6fe3b24d5fd951
JSO-Technologies <JSO.Technologies@gmail.com>
2015-01-22 11:21:00 +0100

    Time server

--- program.js
+++ program.js
@@ -1,46 +1,8 @@
-var printer = require('./printer');
-var http = require('http');
+var net = require('net');
+var strftime = require('strftime')
 
-var self = this;
-var getAndPrintHttpResource = function(url, callback) {
-    http.get(url, function(response) {
-        printer.printHttpResponse(response, callback);
-    });
-};
-
-getAndPrintHttpResource(process.argv[2],
-    getAndPrintHttpResource.bind(self, process.argv[3],
-        getAndPrintHttpResource.bind(self, process.argv[4])));
-
-/**
- Official response
- */
-/*
-var http = require('http')
-var bl = require('bl')
-var results = []
-var count = 0
-
-function printResults () {
-    for (var i = 0; i < 3; i++)
-        console.log(results[i])
-}
-
-function httpGet (index) {
-    http.get(process.argv[2 + index], function (response) {
-        response.pipe(bl(function (err, data) {
-            if (err)
-                return console.error(err)
-
-            results[index] = data.toString()
-            count++
-
-            if (count == 3)
-                printResults()
-        }))
-    })
-}
-
-for (var i = 0; i < 3; i++)
-    httpGet(i)
-*/
+var server = net.createServer(function(socket) {
+    socket.write(strftime('%Y-%m-%d %H:%M', new Date()) + '\n');
+    socket.end();
+});
+server.listen(process.argv[2]);
