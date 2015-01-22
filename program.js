2de64995092d2a395843275658f83b1a5f97e580
JSO-Technologies <JSO.Technologies@gmail.com>
2015-01-22 11:54:14 +0100

    Http uppercaser

--- program.js
+++ program.js
@@ -1,10 +1,17 @@
 var http = require('http');
-var fs = require('fs');
+var map = require('through2-map');
 
 var server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'content-type': 'text/plain' })
+    if(req.method != 'POST') {
+        res.writeHead(400, { 'content-type': 'text/plain' });
+        res.write('Only POST is accepted');
+        res.end();
+    }
 
-    var fileStream = fs.createReadStream(process.argv[3]);
-    fileStream.pipe(res);
+    res.writeHead(200, { 'content-type': 'text/plain' });
+
+    req.pipe(map(function(chunk) {
+        return chunk.toString().toUpperCase();
+    })).pipe(res);
 });
 server.listen(Number(process.argv[2]));
